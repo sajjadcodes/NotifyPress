b749a079cc5469e9de3a94368b462cfc1dfcf776
kamrankcode <kamran.khan@splicedigital.com>
2023-05-24 13:42:28 -0400

    basic structure

---
+++ notifypress.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: NotifyPress
+Description: Simple WordPress notification plugin
+Version:     1.0.0
+Author:      Sajad Hussain
+Plugin URI:  https://sajjadcodes.com/notify-press/
+Text Domain: notifypress
+License:     GPL-2.0+
+Domain Path: /languages
+
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
